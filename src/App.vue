1962fd9466cc94337189f88de0527084bd3e73dd
tflare <taki@tflare.com>
2020-01-17 00:21:44 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,44 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- ログイン時にはサインアウトのリンクを表示 -->
+      <div v-if="user.uid" key="login">
+        [{{ user.email }}]
+        <a href="a" @click="signout">サインアウト</a>
+      </div>
+      <!-- 未ログイン時にはサインインのリンクを表示 -->
+      <div v-else key="logout">
+        <router-link to="/signin">signin</router-link>
+      </div>
     </div>
     <router-view/>
+
   </div>
 </template>
 
+<script>
+// firebase モジュール
+import firebase from 'firebase'
+export default {
+  data() {
+    return {
+      user: {},  // ユーザー情報
+
+    }
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.user = user ? user : {}
+    })
+  },
+  methods: {
+    // ログアウト処理
+    signout() {
+      firebase.auth().signOut()
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
