51846d0f3229e7fa3338b9c0d306c51d9022187f
tflare <taki@tflare.com>
2020-01-17 00:22:40 +0900

    [add]サインイン・サインアウトの機能を実装

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
