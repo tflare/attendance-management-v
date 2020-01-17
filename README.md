679cf8471b8ac3f1fe8e65d696fc6b1654da94c7
tflare <taki@tflare.com>
2020-01-17 21:33:57 +0900

    [add].env.local追加 FirebaseのAPIKEYなどを.env.localに設定し main.jsからは上記で設定した設定を参照するように修正

--- README.md
+++ README.md
@@ -4,7 +4,20 @@
 ```
 npm install
 ```
+### Firebase設定
+```
+プロジェクトのルート.env.localを作成し
+以下を設定すること
 
+VUE_APP_APIKEY=
+VUE_APP_AUTHDOMAIN=
+VUE_APP_DATABASEURL=
+VUE_APP_PROJECTID=
+VUE_APP_STORAGEBUCKET=
+VUE_APP_MESSAGINGSENDERID=
+VUE_APP_APPID=
+VUE_APP_MEASUREMENTID=
+```
 ### Compiles and hot-reloads for development
 ```
 npm run serve
--- src/main.js
+++ src/main.js
@@ -6,14 +6,14 @@ import firebase from 'firebase'
 Vue.config.productionTip = false
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASEURL,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID,
+  measurementId: process.env.VUE_APP_MEASUREMENTID,
 };
 firebase.initializeApp(firebaseConfig);
 
