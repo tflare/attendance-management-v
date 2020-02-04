87a5645f91afd859d4ce61bbf32690839588c7b2
tflare <taki@tflare.com>
2020-02-04 23:28:34 +0900

    [fix]ログインフォームにアイコン追加 ・パスワードを表示・非表示可能に ・コード整理

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,56 +1,28 @@
 <template>
-<v-app id="inspire">
-  <v-content>
-    <v-container
-      class="fill-height"
-      fluid
-    >
-      <v-row
-        align="center"
-        justify="center"
-      >
-        <v-col
-          cols="12"
-          sm="8"
-          md="4"
-        >
-          <v-card class="elevation-12">
-            <v-toolbar
-              color="primary"
-              dark
-              flat
-            >
-              <v-toolbar-title>Login form</v-toolbar-title>
-              <v-spacer />
-
-            </v-toolbar>
-            <v-card-text>
-              <v-form>
-                <v-text-field
-                  label="Login"
-                  name="login"
-                  v-model="username"
-                  type="text"
-                />
-
-                <v-text-field
-                  id="password"
-                  v-model="password"
-                  label="Password"
-                  name="password"
-                  type="password"
-                />
-              </v-form>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer />
-              <v-btn color="primary" @click="signIn">Login</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-content>
+<v-app>
+  <v-card width="500" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">ログイン</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field label="ユーザID" prepend-icon="mdi-account-circle" v-model="username" />
+        <v-text-field
+          label="パスワード"
+          :type="showPassword ? 'text' : 'password'"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+           v-model="password"
+          @click:append="showPassword = !showPassword"
+        />
+      </v-form>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-spacer />
+       <v-btn color="primary" @click="signIn">Login</v-btn>
+    </v-card-actions>
+  </v-card>
 </v-app>
 </template>
 
