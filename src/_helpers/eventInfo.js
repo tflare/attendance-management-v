9bc02654a741f032364f9af38680e6084330b67e
tflare <taki@tflare.com>
2020-02-09 08:09:03 +0900

    [fix]Cloud Functions for Firebaseのregionを設定
    Cloud Functions for Firebaseのregionが設定されておらず、Cross-Origin Resource Sharing (CORS)のエラーが出ていたため、regionを設定した。

--- src/_helpers/eventInfo.js
+++ src/_helpers/eventInfo.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
 export function createEventInfo(eventID) {
+  const message = { "eventID": eventID };
 
-  const attendance2db = firebase.functions().httpsCallable('attendance2db');
-  attendance2db({ "eventID": eventID }).then((res) => {
-    console.log(res); // { message: success }
-  });
+  const functions = firebase.app().functions('asia-northeast1');
+  functions.httpsCallable('attendance2db')(message)
+    .then(result => {
+      console.log('function attendance2db result', result);
+    })
+    .catch(error => {
+      // Error handler //
+      console.log('function attendance2db error', error);
+    });
 
 }
