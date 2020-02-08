b2086914c2b6583b7d27143184ff16d9564eccf4
tflare <taki@tflare.com>
2020-02-08 21:44:10 +0900

    [update]connpassのAPI呼び出しから、Cloud Functions For Firebaseの呼び出しに変更
    今後Cloud Functions For Firebaseの内部からconnpassのAPIを呼び出すように変更予定

--- src/_helpers/eventInfo.js
+++ src/_helpers/eventInfo.js
@@ -1,20 +1,10 @@
 import firebase from 'firebase';
-const axios = require('axios');
 
 export function createEventInfo(eventID) {
 
-  //URLを入れる
-  const url = "https://connpass.com/api/v1/event/?event_id=" + eventID;
-  axios.get(url)
-  .then((response) => {
-    const event = response.data.events[0];
-    firebase.firestore().collection('event').add({
-      eventID: Number(event.eventID),
-      title: event.title,
-      startedAt: event.startedAt,
-      endedAt: event.endedAt,
-      accepted: event.accepted,
-      waiting: event.waiting
-    })
+  const attendance2db = firebase.functions().httpsCallable('attendance2db');
+  attendance2db({ "eventID": eventID }).then((res) => {
+    console.log(res); // { message: success }
   });
+
 }
