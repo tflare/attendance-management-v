590b4a06ebdad4a1e65fed19d0922fe1a2597124
tflare <taki@tflare.com>
2020-02-10 21:02:17 +0900

    [update]Material Design Viewport Breakpointsを使用した画面サイズに合わせた表示の対応

--- src/_helpers/eventInfo.js
+++ src/_helpers/eventInfo.js
@@ -5,12 +5,6 @@ export function createEventInfo(eventID) {
 
   const functions = firebase.app().functions('asia-northeast1');
   functions.httpsCallable('attendance2db')(message)
-    .then(result => {
-      console.log('function attendance2db result', result);
-    })
-    .catch(error => {
-      // Error handler //
-      console.log('function attendance2db error', error);
-    });
+
 
 }
--- src/components/EventUser.vue
+++ src/components/EventUser.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
-    <v-row v-for="(idx, key) in Math.ceil(attendances.length / 5)" :key="key">
-      <v-col v-for="(attendance, key2) in attendances.slice((idx - 1) * 5, idx * 5)" :key="key2">
+    <v-row v-for="(idx, key) in Math.ceil(attendances.length / colNumber)" :key="key">
+      <v-col v-for="(attendance, key2) in attendances.slice((idx - 1) * colNumber, idx * colNumber)" :key="key2">
         {{attendance.userID}} <v-btn small color="primary">出席</v-btn><v-btn small color="error">欠席</v-btn>
       </v-col>
     </v-row>
@@ -25,6 +25,19 @@
       }
     },
 
+    computed: {
+      colNumber: function() {
+        let number;
+        switch (this.$vuetify.breakpoint.name) {
+          case 'xs': number = 1; break;
+          case 'sm': number = 2; break;
+          case 'md': number = 3; break;
+          case 'lg': number = 4; break;
+          case 'xl': number = 5; break;
+        }
+        return number;
+      },
+    },
   }
 
 </script>
