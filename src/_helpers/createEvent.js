5cf299cf421bf69ea22f7f955c00af870e55aa1c
tflare <taki@tflare.com>
2020-02-20 20:19:23 +0900

    [update]イベント追加時、成功・失敗　どちらの場合でもメッセージを出すように修正

--- src/_helpers/createEvent.js
+++ src/_helpers/createEvent.js
@@ -3,6 +3,12 @@ import firebase from 'firebase';
 export function createEvent(eventID) {
   const message = { "eventID": eventID };
 
-  const functions = firebase.app().functions('asia-northeast1');
-  functions.httpsCallable('attendance2db')(message)
+  const attendance2db = firebase.app().functions('asia-northeast1').httpsCallable('attendance2db');
+  attendance2db(message).then(function(result) {
+    const resultMessage = result.data.message;
+    if(resultMessage != null){
+      //neither null nor undefined
+      alert(resultMessage);
+    }
+  });
 }
--- src/_helpers/statisticsUser.js
+++ src/_helpers/statisticsUser.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 
 export function staticsUser(userID) {
-
+  // 一覧表生成機能の方がほしいので方針転換
   let attendanceCount = 0;
   let absenceCount = 0;
   let presenterCount = 0;
-  var user = firebase.firestore().collectionGroup("users").where('userID', '==', userID);
+  var user = firebase.firestore().collectionGroup("users").where('id', '==', userID);
   user.get().then(function (querySnapshot) {
       querySnapshot.forEach(function (doc) {
         if(doc.attendance == true){
